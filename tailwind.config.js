794cfa4acebe4ae2390c8f8d9a3a57c1e794f9d1
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2023-04-27 10:17:30 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sd: "350px",
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "740px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      bone: "#D7D9CE",
+      lightgreen: "#6F8C75",
+      midgreen: "#4A5E4E",
+      darkgreen: "#062600",
+    },
+  },
+  plugins: [],
+};
