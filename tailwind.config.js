89c9bbf21684ed0fc6d8aa546f4ab5846dc5dbb3
Alexandru <a.muresan8533@gmail.com>
2024-08-23 17:25:18 -0400

    change colors and update wording

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
-    screens: {
-      sd: "350px",
+	content: ['./**/*.{html,js}'],
+	theme: {
+		extend: {},
+		screens: {
+			sd: '350px',
 
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
 
-      md: "740px",
-      // => @media (min-width: 768px) { ... }
+			md: '740px',
+			// => @media (min-width: 768px) { ... }
 
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
 
-      "2xl": "1536px"
-      // => @media (min-width: 1536px) { ... }
-    },
-    colors: {
-      ...colors,
-      bone: "#D7D9CE",
-      lightgreen: "#6F8C75",
-      midgreen: "#4A5E4E",
-      darkgreen: "#062600"
-    }
-  },
-  plugins: [require("tailwind-scrollbar-hide")]
+			'2xl': '1536px'
+			// => @media (min-width: 1536px) { ... }
+		},
+		colors: {
+			...colors,
+			bone: '#fff7eb',
+			lightgreen: '#baa89b',
+			midgreen: '#b0896a',
+			darkgreen: '#DC965A'
+		}
+	},
+	plugins: [require('tailwind-scrollbar-hide')]
 };
